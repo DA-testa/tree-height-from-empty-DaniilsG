9bd0aa59e9c4a4f6e3a0a21c2293c7ef3277ad30
DaniilsG <116102332+DaniilsG@users.noreply.github.com>
2023-03-05 19:26:34 +0000

    uzd.2. Daniils Ginglats

--- tree_height.py
+++ tree_height.py
@@ -2,32 +2,63 @@
 
 import sys
 import threading
-import numpy
+
+def compute_height(a, parents):
+    root = None
+    position = [[] for _ in range(a)]
+    for i in range(a):
+        if parents[i] == -1:
+            root = i
+        else:
+            position[parents[i]].append(i)
 
 
-def compute_height(n, parents):
-    # Write this function
-    max_height = 0
-    # Your code here
-    return max_height
+
+    def max_height(b):
+        height = 1
+        if not position[b]:
+            return height
+        else:
+
+            for child in position[b]:
+                height = max(height, max_height(child))
+            return height + 1
+    return max_height(root)
+
+
 
 
 def main():
-    # implement input form keyboard and from files
-    
-    # let user input file name to use, don't allow file names with letter a
-    # account for github input inprecision
-    
-    # input number of elements
-    # input values in one variable, separate with space, split these values in an array
-    # call the function and output it's result
-    pass
+    text = input("Enter F - I: ")
+    if "I" in text:
+        n = int(input())
+        parents = list(map(int, input().split()))
 
-# In Python, the default limit on recursion depth is rather low,
-# so raise it here for this problem. Note that to take advantage
-# of bigger stack, we have to launch the computation in a new thread.
-sys.setrecursionlimit(10**7)  # max depth of recursion
-threading.stack_size(2**27)   # new thread will get stack of such size
-threading.Thread(target=main).start()
-main()
-# print(numpy.array([1,2,3]))
+    elif "F" in text:
+
+        filename = input()
+        file_path = './test/' + filename
+
+        if "a" not in filename:
+            try:
+                with open(file_path) as f:
+
+                    n = int(f.readline())
+                    parents = list(map(int, f.readline().split()))
+            except Exception as e:
+                print("Error:", str(e))
+                return
+        else:
+
+            print("invalid filename")
+            return
+    print(compute_height(n, parents))
+
+
+if __name__ == '__main__':
+    # In Python, the default limit on recursion depth is rather low,
+    # so raise it here for this problem. Note that to take advantage
+    # of bigger stack, we have to launch the computation in a new thread.
+    sys.setrecursionlimit(10 ** 7)  # max depth of recursion
+    threading.stack_size(2 ** 27)  # new thread will get stack of such size
+    threading.Thread(target=main).start()
